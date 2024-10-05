5637baa2fdaba35432a772a8a80abb9574d58757
Thank8484 <131605383+Thank8484@users.noreply.github.com>
2024-10-05 23:10:12 +0700

    Added Http

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ try {
   for (const key in cronSubjects) {
     if (Object.prototype.hasOwnProperty.call(cronSubjects, key)) {
       const daySubjects = cronSubjects[key];
+      console.log(
+        `Register Cron job: ${subject.name} at ${subject.time} on ${key}`
+      );
       daySubjects.forEach((subject) => {
-        console.log(
-          `Register Cron job: ${subject.name} at ${subject.time} on ${key}`
-        );
         cron.schedule(subject.cron, () => {
           sendMessage(`${subject.time} ${subject.name}`, subject.name);
         });
